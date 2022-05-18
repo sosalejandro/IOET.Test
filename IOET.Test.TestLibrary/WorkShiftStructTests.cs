7bc7eeaa9a8e3598af11883f8dfd089b365a2aab
Alejandro sosa <developer@sosalejandro.onmicrosoft.com>
2022-05-18 12:34:29 -0400

    Add CreateFromTime valid and invalid input tests

--- IOET.Test.TestLibrary/WorkShiftStructTests.cs
+++ IOET.Test.TestLibrary/WorkShiftStructTests.cs
@@ -1,4 +1,5 @@
-﻿using IOET.Test.Library.Models;
+﻿using IOET.Test.Library.Exceptions;
+using IOET.Test.Library.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,8 +22,47 @@ public class WorkShiftStructTests
         var workShiftStruct = CreateWorkShiftStruct(timeInput);
 
         // Assert        
-        Assert.Equal(TimeSpan.Parse("10:15".ToString()), workShiftStruct.Entry);
-        Assert.Equal(TimeSpan.Parse("15:40".ToString()), workShiftStruct.Exit);
+        Assert.Equal(TimeSpan.Parse("10:15"), workShiftStruct.Entry);
+        Assert.Equal(TimeSpan.Parse("15:40"), workShiftStruct.Exit);
+    }
 
+    [Theory]
+    [InlineData("MO10:00-12:00", "10:00", "12:00")]
+    [InlineData("TU10:00-12:00", "10:00", "12:00")]
+    [InlineData("TH01:00-03:00", "01:00", "03:00")]
+    [InlineData("SA14:00-18:00", "14:00", "18:00")]
+    [InlineData("SU20:00-21:00", "20:00", "21:00")]
+    public void CreateFromTime_CreatesANewInstance_WhenInputIsValid(
+        string timeInput, 
+        string entryTime, 
+        string exitTime)
+    {
+        // Arrange
+        Func<string, WorkShiftStruct> CreateWorkShiftStruct = (input)
+           => WorkShiftStruct.CreateFromTime(input);
+
+        // Act
+        var workShiftStruct = CreateWorkShiftStruct(timeInput);
+
+        // Assert        
+        Assert.Equal(TimeSpan.Parse(entryTime), workShiftStruct.Entry);
+        Assert.Equal(TimeSpan.Parse(exitTime), workShiftStruct.Exit);
+    }
+
+    [Theory]
+    [InlineData("M10:00-12:00")]
+    [InlineData("MAA10:00-12:00")]
+    [InlineData("0:00-12:00")]
+    [InlineData("M10:0012:00")]
+    [InlineData("M10:00-12000")]
+    public void CreateFromTime_Should_Throw_WhenInputIsInvalid(string timeInput)
+    {
+        // Arrange
+        Func<string, WorkShiftStruct> CreateWorkShiftStruct = (input)
+           => WorkShiftStruct.CreateFromTime(input);
+
+        // Act & Assert
+        Assert.Throws<InvalidInputException>(
+            () => CreateWorkShiftStruct(timeInput));        
     }
 }
