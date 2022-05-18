697ed3f57f00d56eb42365d821d39e4ede1b5eaa
Alejandro sosa <developer@sosalejandro.onmicrosoft.com>
2022-05-18 06:39:14 -0400

    Add employee model

---
+++ IOET.Test.Library/Models/Employee.cs
@@ -0,0 +1,48 @@
+﻿using IOET.Test.Library.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOET.Test.Library.Models;
+public record Employee
+{
+    public string Name { get; set; }
+    public HashSet<EmployeeWorkShift> WorkShifts { get; private set; }
+
+    public Employee(string name)
+    {
+        Name = name;
+        WorkShifts = new();
+    }
+
+    internal Employee(string name, ICollection<EmployeeWorkShift> workShifts)
+    {
+        Name = name;
+        WorkShifts = workShifts.ToHashSet();
+    }
+
+    public static Employee CreateFromInput(string input)
+    {
+        string name = input.GetName();
+        string namePattern = $"{name}=";
+        input = input[(namePattern.Length)..];
+
+
+        ICollection<EmployeeWorkShift> workShifts = new HashSet<EmployeeWorkShift>();
+        var entries = input.ParseIntoList();
+
+        foreach (string entry in entries)
+        {
+            workShifts.Add(entry.ParseIntoEmployeeWorkShift());
+        }
+
+        return new Employee(name, workShifts);
+    }
+
+    public void AddWorkShifts(ICollection<EmployeeWorkShift> workShifts)
+    {
+        WorkShifts.UnionWith(workShifts);
+    }
+}
