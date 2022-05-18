6d0e65f1dc421015d39dc87602cd64c30b5ce37e
Alejandro sosa <developer@sosalejandro.onmicrosoft.com>
2022-05-18 06:39:27 -0400

    Add IScheduleComparer interface

---
+++ IOET.Test.Library/Interfaces/IScheduleComparer.cs
@@ -0,0 +1,15 @@
+﻿using IOET.Test.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOET.Test.Library.Interfaces;
+public interface IScheduleComparer
+{
+    int CompareWorkShifts(IEnumerable<EmployeeWorkShift> employeeWorkShift1, IEnumerable<EmployeeWorkShift> employeeWorkShift2);
+    IEnumerable<Employee> CreateEmployeesFromInputs(List<string> inputs);
+    IEnumerable<string> GetScheduleResults(IEnumerable<Employee> employees, Employee start);
+    List<string> ParseIntoScheduleStringList(string textFile);
+}
