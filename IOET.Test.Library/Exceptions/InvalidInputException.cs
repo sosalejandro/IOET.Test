991ffda7bf43fe05fc65c845cef3d519c7457ccd
Alejandro sosa <developer@sosalejandro.onmicrosoft.com>
2022-05-18 11:59:50 -0400

    Add InvalidInputException class

---
+++ IOET.Test.Library/Exceptions/InvalidInputException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace IOET.Test.Library.Exceptions;
+
+[Serializable]
+public class InvalidInputException : Exception
+{
+    public InvalidInputException()
+    {
+    }
+
+    public InvalidInputException(string? message) : base(message)
+    {
+    }
+
+    public InvalidInputException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected InvalidInputException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
