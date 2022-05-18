17350828950081625ebb71ee7a4b488c5c445962
Alejandro sosa <developer@sosalejandro.onmicrosoft.com>
2022-05-18 06:21:49 -0400

    Add work shifts models

---
+++ IOET.Test.Library/Models/EmployeeWorkShift.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOET.Test.Library.Models;
+
+public record EmployeeWorkShift(DayOfWeek DayOfWeek, TimeSpan Entry, TimeSpan Exit)
+{
+}
+
---
+++ IOET.Test.Library/Models/WorkShiftStruct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOET.Test.Library.Models;
+
+public struct WorkShiftStruct
+{
+    public TimeSpan Entry;
+    public TimeSpan Exit;
+
+    internal WorkShiftStruct(ReadOnlySpan<char> entry, ReadOnlySpan<char> exit)
+    {
+        Entry = TimeSpan.Parse(entry.ToString());
+        Exit = TimeSpan.Parse(exit.ToString());
+    }
+
+    public static WorkShiftStruct CreateFromTime(string input)
+    {
+        var time = input[2..].AsSpan();
+
+        var middle = time.IndexOf("-");
+
+        var entry = time[..middle];
+
+        var exit = time.Slice(middle + 1);
+
+        return new WorkShiftStruct(entry, exit);
+    }
+}
