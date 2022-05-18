796d01b4e67b5d9eccccdef52d982b6c11225455
Alejandro sosa <developer@sosalejandro.onmicrosoft.com>
2022-05-18 11:57:43 -0400

    Add documentation and refactor

--- IOET.Test.Library/Extensions/StringExtensions.cs
+++ IOET.Test.Library/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using IOET.Test.Library.Models;
+﻿using IOET.Test.Library.Exceptions;
+using IOET.Test.Library.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,44 +10,20 @@ using System.Threading.Tasks;
 namespace IOET.Test.Library.Extensions;
 
 public static class StringExtensions
-{
-    public static List<string> ParseIntoList(this string input)
-    {
-        var results = input.Split(",").ToList();
-
-        return results;
-    }
-
-    public static EmployeeWorkShift ParseIntoEmployeeWorkShift(this string input)
-    {
-        var dayOfWeek = GetDayOfTheWeek(input);
-        WorkShiftStruct workShiftStruct = WorkShiftStruct.CreateFromTime(input);
-
-        EmployeeWorkShift empShift = new(dayOfWeek, workShiftStruct.Entry, workShiftStruct.Exit);
-
-        return empShift;
-    }
-
+{    
+    /// <summary>
+    /// Gets the name from given input matching a Regex Pattern
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidInputException"></exception>
     public static string GetName(this string input)
-    {        
-        var name = Regex.Match(input, "[a-zA-Z]+=").Value;
-        return name[..^1];
-    }
-
-    private static DayOfWeek GetDayOfTheWeek(this string input)
     {
-        // NOTES: It could be simplified to use only the first two letters as the case but it would required as setup to 
-        // create a variable with the sliced string before hand. It might run quicker, thought. 
-        return input switch
-        {
-            string _ when Regex.Match(input, "MO").Success => DayOfWeek.Monday,
-            string _ when Regex.Match(input, "TU").Success => DayOfWeek.Tuesday,
-            string _ when Regex.Match(input, "WE").Success => DayOfWeek.Wednesday,
-            string _ when Regex.Match(input, "TH").Success => DayOfWeek.Thursday,
-            string _ when Regex.Match(input, "FR").Success => DayOfWeek.Friday,
-            string _ when Regex.Match(input, "SA").Success => DayOfWeek.Saturday,
-            string _ when Regex.Match(input, "SU").Success => DayOfWeek.Sunday,
-            _ => throw new NotImplementedException()
-        };
-    }
+        string pattern = "[a-zA-Z]+=";
+        if (!Regex.Match(input, pattern).Success)
+            throw new InvalidInputException("Invalid input to parse name.");        
+
+        var name = Regex.Match(input, pattern).Value;
+        return name[..^1];
+    }   
 }
